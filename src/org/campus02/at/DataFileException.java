c34fe5402e5bd23665193aa776e85896ebc9149c
patrick-putz <Putz.Patrick.1996@gmail.com>
2022-05-16 19:12:53 +0200

    DataFileException

---
+++ src/org/campus02/at/DataFileException.java
@@ -0,0 +1,9 @@
+package org.campus02.at;
+
+public class DataFileException extends Exception{
+
+    public DataFileException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
